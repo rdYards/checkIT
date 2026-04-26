d6739a9fef9b3719ba7fe91da127bd77a45fd190
rdYards <rdyards@protonmail.com>
2026-04-25 21:39:15 -0500

    Update: Fix gscheme
    Added gscheme to binary

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod app;
 mod data;
-mod ui;
 mod p2p;
+mod ui;
 
-use adw::{Application, gio, gio::Resource, gio::prelude::*, glib};
-use std::path::Path;
+use adw::{
+    Application, gio,
+    gio::Resource,
+    prelude::{ApplicationExt, ApplicationExtManual},
+};
+use glib;
 
 const APP_ID: &str = "org.gtk_rs.CheckIT";
 const DEFAULT_FILE_PATH: &str = "~/";
@@ -12,8 +16,9 @@ const DEFAULT_FILE_PATH: &str = "~/";
 #[tokio::main]
 async fn main() -> glib::ExitCode {
     // Load resources from the output directory
-    let resource_path = Path::new(env!("OUT_DIR")).join("data/checkit.gresource");
-    let res = Resource::load(resource_path).expect("Failed to load resources");
+    let res_bytes = include_bytes!(concat!(env!("OUT_DIR"), "/data/checkit.gresource"));
+    let bytes = glib::Bytes::from(&res_bytes[..]); // Convert to &[u8]
+    let res = Resource::from_data(&bytes).expect("Failed to load resources");
     gio::resources_register(&res);
 
     let app = Application::builder().application_id(APP_ID).build();
@@ -21,5 +26,6 @@ async fn main() -> glib::ExitCode {
     app.connect_activate(|app| {
         app::build_app(app);
     });
+
     app.run()
 }
